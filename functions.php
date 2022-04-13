0485deb4e7c41f98fe658704fed2d121ed749c62
Jonathan Bossenger <jonathanbossenger@gmail.com>
2022-04-13 10:03:51 +0200

    Fixes
    Formatting code to match WPCS
    Adding text domains for i18n

--- functions.php
+++ functions.php
@@ -1,21 +1,22 @@
-<?php 
+<?php
 
 function my_acf_init_block_types() {
-	
+
 	// Check function exists.
-	if( function_exists('acf_register_block_type') ) {
-		
+	if ( function_exists( 'acf_register_block_type' ) ) {
+
 		// register a testimonial block.
-		acf_register_block_type(array(
-			'name'              => 'testimonial',
-			'title'             => __('Testimonial'),
-			'description'       => __('A custom testimonial block.'),
-			'render_template'   => 'template-parts/blocks/testimonial/testimonial.php',
-			'category'          => 'formatting',
-			'icon'              => 'admin-comments',
-			'keywords'          => array( 'testimonial', 'quote' ),
-		));
+		acf_register_block_type( array(
+			'name'            => 'testimonial',
+			'title'           => __( 'Testimonial', 'hellfish-media' ),
+			'description'     => __( 'A custom testimonial block.', 'hellfish-media' ),
+			'render_template' => 'template-parts/blocks/testimonial/testimonial.php',
+			'category'        => 'formatting',
+			'icon'            => 'admin-comments',
+			'keywords'        => array( 'testimonial', 'quote' ),
+		) );
 	}
 
-} 
-add_action('acf/init', 'my_acf_init_block_types');
+}
+
+add_action( 'acf/init', 'my_acf_init_block_types' );
